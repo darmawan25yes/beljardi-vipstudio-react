d12fbc1e95ba98081ec8c2502a501bb4f3e0d267
AwanCahaya <darmawan25yes@outlook.co.id>
2025-11-06 10:30:06 +0700

    menambahkan komponen baru dgn if-else & menambah komentar
    1. menambahkan cara baru dengan membuat komponen baru(Navigasi) yg menerapkan if-else
    
    2.  serta menambahkan komentar komentar

--- src/components/Layouts/AutLayout.jsx
+++ src/components/Layouts/AutLayout.jsx
@@ -8,13 +8,18 @@ const AutLayout = (props) => {
         <div className="w-15 py-3">
           <IconBIM />
         </div>
+        {/* text judul login/register */}
         <h1 className="text-3xl font-bold mb-2 text-blue-600">{title}</h1>
+        {/* text di bawa tombol */}
         <p className="font-medium text-slate-500">
           welcome, please enter your details
         </p>
+        {/* ini adalah komponen yang di kirim dari pages */}
         {children}
+        {/* ini jika menggunakan cara membuat komponen lain dengan if else */}
+        <Navigasi title={title} />
         <p className="font-medium text-sm text-slate-500 text-center py-2">
-          {title === "Login" ? " Don't have account? " : "Have account?"}
+          {title === "Login" ? " Don't have account? " : "Have account? "}
 
           {title === "Login" && (
             <Link to="/register" className="text-blue-700">
@@ -31,6 +36,29 @@ const AutLayout = (props) => {
       </div>
     </div>
   );
+};
+
+const Navigasi = (props) => {
+  const { title } = props;
+  if (title === "Login") {
+    return (
+      <p className="font-medium text-sm text-slate-500 text-center py-2">
+        Don't have account ?{" "}
+        <Link to="/register" className="text-red-700">
+          Sign up
+        </Link>
+      </p>
+    );
+  } else {
+    return (
+      <p className="font-medium text-sm text-slate-500 text-center py-2">
+        Have account ?{" "}
+        <Link to="/login" className="text-red-700">
+          Login
+        </Link>
+      </p>
+    );
+  }
 };
 
 export default AutLayout;
