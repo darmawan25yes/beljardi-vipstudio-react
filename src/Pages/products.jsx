d9a3b8645da4276c236879508abc79b158949160
AwanCahaya <darmawan25yes@outlook.co.id>
2025-11-06 20:54:58 +0700

    memisahkan bagian Card Product jadi nested component
    di pisahkan jadi 3 komponen
    1. header
    2. body
    3. footer

--- src/Pages/products.jsx
+++ src/Pages/products.jsx
@@ -1,9 +1,28 @@
-import Button from "../components/Elements/Button";
 import CardProduct from "../components/Fragments/CardProduct";
 const ProductsPage = () => {
   return (
     <div className="flex justify-center py-5">
-      <CardProduct />
+      <CardProduct>
+        <CardProduct.Header image="/images/si-surga.png"></CardProduct.Header>
+        <CardProduct.Body title="Si Surga">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ex
+          blanditiis in minus! Impedit, aut necessitatibus. Non assumenda,
+          impedit commodi vitae iusto velit dolor, dolore debitis neque magnam
+          quod distinctio.
+        </CardProduct.Body>
+        {/* cara menggunakannya dengan dot */}
+        <CardProduct.Footer price="Rp. 25.000"></CardProduct.Footer>
+      </CardProduct>
+      <CardProduct>
+        <CardProduct.Header image="/images/si-surga.png"></CardProduct.Header>
+        <CardProduct.Body title="Si Surga">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ex
+          blanditiis in minus! Impedit, aut necessitatibus. Non assumenda,
+          impedit commodi vitae iusto velit dolor, dolore debitis neque magnam
+          quod distinctio.
+        </CardProduct.Body>
+        <CardProduct.Footer price="Rp. 25.000"></CardProduct.Footer>
+      </CardProduct>
     </div>
   );
 };
--- src/components/Fragments/CardProduct.jsx
+++ src/components/Fragments/CardProduct.jsx
@@ -1,31 +1,45 @@
 import Button from "../Elements/Button";
-const CardProduct = () => {
+const CardProduct = (props) => {
+  const { children } = props;
   return (
-    <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow">
-      <a href="g">
-        <img
-          src="/images/si-surga.png"
-          alt="product"
-          className="p-8 rounded-t-lg"
-        />
-      </a>
-      <div className="px-5 pb-5">
-        <h5 className="text-xl font-semibold tracking-tight text-white">
-          Si Surga
-        </h5>
-        <p className="text-m text-white">
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Incidunt ex
-          blanditiis in minus! Impedit, aut necessitatibus. Non assumenda,
-          impedit commodi vitae iusto velit dolor, dolore debitis neque magnam
-          quod distinctio.
-        </p>
-      </div>
-      <div className="flex items-center justify-between px-5 pb-5">
-        <span className="text-xl font-bold text-white">Rp. 25.000</span>
-        <Button>Daftar</Button>
-      </div>
+    <div className="w-full max-w-sm bg-gray-800 border-gray-700 rounded-lg shadow mx-2">
+      {children}
     </div>
   );
 };
 
+const Body = (props) => {
+  const { children, title } = props;
+  return (
+    <div className="px-5 pb-5">
+      <h5 className="text-xl font-semibold tracking-tight text-white">
+        {title}
+      </h5>
+      <p className="text-m text-white">{children}</p>
+    </div>
+  );
+};
+
+const Header = (props) => {
+  const { image } = props;
+  return (
+    <a href="g">
+      <img src={image} alt="product" className="p-8 rounded-t-lg" />
+    </a>
+  );
+};
+
+const Footer = (props) => {
+  const { price } = props;
+  return (
+    <div className="flex items-center justify-between px-5 pb-5">
+      <span className="text-xl font-bold text-white">{price}</span>
+      <Button>Daftar</Button>
+    </div>
+  );
+};
+// harus di definisikan seperti ini agar komponen body,header, dan footer ikut di eksport dalam CardProduct
+CardProduct.Body = Body;
+CardProduct.Header = Header;
+CardProduct.Footer = Footer;
 export default CardProduct;
